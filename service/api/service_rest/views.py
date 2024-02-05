98a754cfb4bdf689767c9d7f7c3f364d4c5c5a4e
Micah <micah.a.awtrey@gmail.com>
2024-02-05 13:55:04 -0700

    Created List and Create Technicians API

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .models import *
+from common.json import ModelEncoder
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id"
+    ]
+
+class AutomobileVOListEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "vin",
+        "sold"
+    ]
+
+class AppointmentListEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician"
+    ]
+    encoders = {
+        "technician": TechnicianListEncoder
+    }
+
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        new_tech = Technician.objects.create(**content)
+        return JsonResponse(
+            {"new_tech": new_tech},
+            encoder=TechnicianListEncoder
+        )
